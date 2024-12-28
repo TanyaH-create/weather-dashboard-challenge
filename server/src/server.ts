f88ebfa8ef71126f89bb7ad087d3dfdc13ada39f
Tanya <dougtanyah@gmail.com>
2024-12-27 20:55:11 -0500

    feat: main update server.ts with middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
+
+
 dotenv.config();
 
 // Import the routes
@@ -10,8 +12,11 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
+app.use(express.static('../client/dist'));
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
+app.use(express.json()); // Parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
 // TODO: Implement middleware to connect the routes
 app.use(routes);
